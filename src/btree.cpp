54b611fe5bce2d3a58e68afd2eea476e6aa7744a
Max J <mkjohnson9@wisc.edu>
2022-03-03 13:43:21 -0600

    More constructor stuff

--- src/btree.cpp
+++ src/btree.cpp
@@ -16,85 +16,92 @@
 #include "exceptions/file_not_found_exception.h"
 #include "exceptions/end_of_file_exception.h"
 
-
 //#define DEBUG
 
 namespace badgerdb
 {
 
-// -----------------------------------------------------------------------------
-// BTreeIndex::BTreeIndex -- Constructor
-// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
+	// BTreeIndex::BTreeIndex -- Constructor
+	// -----------------------------------------------------------------------------
 
-BTreeIndex::BTreeIndex(const std::string & relationName,
-		std::string & outIndexName,
-		BufMgr *bufMgrIn,
-		const int attrByteOffset,
-		const Datatype attrType)
-{
-	// Index File Name
-	std::ostringstream idxStr;
-	idxStr << relationName << '.' << attrByteOffset;
-	std::string indexName = idxStr.str();
+	BTreeIndex::BTreeIndex(const std::string &relationName,
+						   std::string &outIndexName,
+						   BufMgr *bufMgrIn,
+						   const int attrByteOffset,
+						   const Datatype attrType)
+	{
+		// Index File Name
+		std::ostringstream idxStr;
+		idxStr << relationName << '.' << attrByteOffset;
+		std::string indexName = idxStr.str();
 
-	// check if the index file exists yet
-	// Have to fix this
-	try {
-		File indexFile = new File(indexName, true);
-	} catch (FileExistsException ex) {
+		// pointer to the indexFile
+		BlobFile *indexFile = nullptr;
 
+		// check if the index file exists yet
+		// Have to fix this?
+		try
+		{
+			indexFile = new BlobFile(indexName, true);
+
+			// Use the FileScan class to build the index on the input relation
+			FileScan *scanner = new FileScan(relationName, bufMgrIn);
+
+			// get records from the file, come up with a key
+		}
+		catch (FileExistsException ex)
+		{ // if the file already exists, do nothing, keep file open
+		}
+		catch (FileNotFoundException ex)
+		{ // relation does not exist (thrown from FileScan instantiation)
+		}
+
+		// set the out index name
+		outIndexName = indexName;
 	}
 
-	// set hte out name
-	outIndexName = indexName;
-}
+	// -----------------------------------------------------------------------------
+	// BTreeIndex::~BTreeIndex -- destructor
+	// -----------------------------------------------------------------------------
 
+	BTreeIndex::~BTreeIndex()
+	{
+	}
 
-// -----------------------------------------------------------------------------
-// BTreeIndex::~BTreeIndex -- destructor
-// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
+	// BTreeIndex::insertEntry
+	// -----------------------------------------------------------------------------
 
-BTreeIndex::~BTreeIndex()
-{
-}
+	void BTreeIndex::insertEntry(const void *key, const RecordId rid)
+	{
+	}
 
-// -----------------------------------------------------------------------------
-// BTreeIndex::insertEntry
-// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
+	// BTreeIndex::startScan
+	// -----------------------------------------------------------------------------
 
-void BTreeIndex::insertEntry(const void *key, const RecordId rid) 
-{
+	void BTreeIndex::startScan(const void *lowValParm,
+							   const Operator lowOpParm,
+							   const void *highValParm,
+							   const Operator highOpParm)
+	{
+	}
 
-}
+	// -----------------------------------------------------------------------------
+	// BTreeIndex::scanNext
+	// -----------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------
-// BTreeIndex::startScan
-// -----------------------------------------------------------------------------
+	void BTreeIndex::scanNext(RecordId &outRid)
+	{
+	}
 
-void BTreeIndex::startScan(const void* lowValParm,
-				   const Operator lowOpParm,
-				   const void* highValParm,
-				   const Operator highOpParm)
-{
-
-}
-
-// -----------------------------------------------------------------------------
-// BTreeIndex::scanNext
-// -----------------------------------------------------------------------------
-
-void BTreeIndex::scanNext(RecordId& outRid) 
-{
-
-}
-
-// -----------------------------------------------------------------------------
-// BTreeIndex::endScan
-// -----------------------------------------------------------------------------
-//
-void BTreeIndex::endScan() 
-{
-
-}
+	// -----------------------------------------------------------------------------
+	// BTreeIndex::endScan
+	// -----------------------------------------------------------------------------
+	//
+	void BTreeIndex::endScan()
+	{
+	}
 
 }
